0977355c10d01f4b77840f5edb2b54bb989f267b
eXtremeSHOK.com <admin@extremeshok.com>
2015-09-03 12:37:51 +0200

    Remove Ban of multiple IP addresses

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fail2ban-remove-ban
 Remove Ban / Unban an IP from all Fail2ban Jails
+
+## Usage
+###Remove Ban / Unban an IP from all Fail2ban Jails
+fail2ban-remove-ban.sh <ip-address-to-unban>
+
+###Remove Ban of multiple IP addresses
+fail2ban-remove-ban.sh <ip-1> <ip-2> <ip-3>
+
--- fail2ban-remove-ban.sh
+++ fail2ban-remove-ban.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Fail2ban IP Remove Ban / Unban Script
-# Version 1.0
+# Version 2.0
 # 03 September 2015
 # Copyright (c) Adrian Jon Kriel : root-at-extremecooling-dot-org
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 ####################
 #
 # Remove Ban / Unban an IP from all Fail2ban Jails
+# Usage: fail2ban-remove-ban.sh <ip-address-to-unban>
+#
+# Remove Ban of multiple IP addresses
+# Usage: fail2ban-remove-ban.sh <ip-1> <ip-2> <ip-3>
 #
 ####################
-
 if [[ ! "$1" ]]; then
     echo "Usage: ${0##*/} <ip-address-to-unban>";
+    echo "Multiple IP: ${0##*/} <ip1> <ip2> <ip3> <ip4>";
 	exit
 fi
 
-if [[ ! $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    echo "ERROR: invalid IP $1";
-	exit
-fi
-
-JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g'`
-for JAIL in $JAILS
+for var in "$@"
 do
-  RESULT=`fail2ban-client set $JAIL unbanip 196.210.84.111 2>&1`
+	if [[ $var =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+	    JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g'`
+		for JAIL in $JAILS
+		do
+		  RESULT=`fail2ban-client set $JAIL unbanip 196.210.84.111 2>&1`
 
-  if [[ "$RESULT" == *"NOK"* ]]; then
-    echo "IP NOT banned in $JAIL";
-  else
-     echo "Removed IP ban from $JAIL"
-  fi
+		  if [[ "$RESULT" == *"NOK"* ]]; then
+		    echo "$var NOT banned in $JAIL";
+		  else
+		     echo "Removed $var ban from $JAIL"
+		  fi
+		done
+	else
+		echo "ERROR: invalid IP $var";
+
+	fi
 done
+
